18e65c3d6dd0adb1cd46d948ca807bd89214f67e
GabrielFraga <gabrielbff2006@gmail.com>
2025-08-27 19:53:23 -0300

    fix: adição de 'setProperty' na classe Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,8 @@ public class Main {
 		
 		Order o = new Order();
 		o.add(new Item("Café", 8.0, 2)); // 16
-		o.add(new Item("Bolo", 12.5, 1)); // 12.5 -> subtotal 28.5
+		o.add(new Item("Bolo", 12.5, 1)); // 12.5
+        System.setProperty("tax.rate", "0.10");// -> subtotal 28.5
 		String receipt = new ReceiptService().generate(o);
 		System.out.println(receipt);
 		
